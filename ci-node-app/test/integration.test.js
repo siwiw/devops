9b89643222d317e89c7b58be04b8d0973b7dbe66
siwikrt <153203390+siwiw@users.noreply.github.com>
2026-03-31 23:46:29 +0700

    Add files via upload

---
+++ ci-node-app/test/integration.test.js
@@ -0,0 +1,7 @@
+const request = require("supertest");
+const app = require("../app");
+test("GET / should return Hello CI Pipeline", async () => {
+  const res = await request(app).get("/");
+  expect(res.statusCode).toBe(200);
+  expect(res.text).toBe("Hello CI Pipeline!");
+});
