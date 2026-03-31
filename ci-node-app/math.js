c2d512af8bece8ffbdee3f915053575a58167075
siwikrt <153203390+siwiw@users.noreply.github.com>
2026-03-31 23:59:41 +0700

    Add files via upload

---
+++ ci-node-app/math.js
@@ -0,0 +1,17 @@
+function tambah(a, b) {
+  return a + b;
+}
+
+function kurang(a, b) {
+  return a - b;
+}
+
+function kali(a, b) {
+  return a * b;
+}
+
+function bagi(a, b) {
+  return a / b;
+}
+
+module.exports = { tambah, kurang, kali, bagi };
